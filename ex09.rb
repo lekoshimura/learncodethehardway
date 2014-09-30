772b2cc84a699045e7522347504b70d04931550a
lekoshimura <leonardo.koshimura@gmail.com>
2014-09-30 20:35:48 -0300

    ex10

---
+++ ex09.rb
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+Tem alguma coisa aqui
+que pode espalhar-se por várias linhas
+várias mesmo
+tipo, umas 3 ou 4
+ou até mais
+"""

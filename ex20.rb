3ff3aef1d8a1a19da931ae287427555fb94f7d63
lekoshimura <leonardo.koshimura@gmail.com>
2014-10-02 18:11:09 -0300

    ex20

---
+++ ex20.rb
@@ -0,0 +1,32 @@
+input_file = ARGV.first
+
+def print_all(f)
+    puts f.read
+end
+
+def rewind(f)
+    f.seek(0)
+end
+
+def print_a_line(line_count, f)
+    puts "#{line_count}, #{f.gets.chomp}"
+end
+
+current_file = open(input_file)
+
+puts "Primeiro, vamos imprimir o arquivo inteiro:\n"
+print_all(current_file)
+
+puts "Agora vamos voltar, como se fosse uma fita."
+rewind(current_file)
+
+puts "Vamos imprimir 3 linhas:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line +=  + 1
+print_a_line(current_line, current_file)
+
+current_line +=  + 1
+print_a_line(current_line, current_file)
